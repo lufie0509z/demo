a9263336396f8d98b3ab0db6252895f374f6f4db
lufie <lufie@lufiedeMacBook-Air.local>
2023-12-05 13:29:25 +0800

    8.12 modify some implementations

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -7,10 +7,10 @@ import { useEffect } from "react";
 
 export default observer(function ActivityDashboard() {
     const {activityStore} = useStore();
-
+    const {loadActivities, activityRegistry} = activityStore;
     useEffect(() => {
-      activityStore.loadActivities();
-    }, [activityStore])
+      if (activityRegistry.size <= 1) loadActivities();
+    }, [loadActivities, activityRegistry.size])
   
     if (activityStore.loadingInitial) return <LoadingComponent content='Loading app...' />
   
