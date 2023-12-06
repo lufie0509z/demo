2c41305274f9dc25fc8ec97cca74b8890998e2c2
lufie <lufie@lufiedeMacBook-Air.local>
2023-12-06 23:12:12 +0800

    bugs: date is null

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -12,15 +12,19 @@ import MyTextArea from '../../../app/common/form/MyTextArea';
 import MySelectInput from '../../../app/common/form/MySelectInput';
 import { categoryOptions } from '../../../app/common/options/categoryOptions';
 import MyDateInput from '../../../app/common/form/MyDateInput';
+import { v4 as uuid } from 'uuid';
 import '../style.css'
 
 export default observer(function ActivityForm() {
     const {activityStore} = useStore();
-    const {loading, loadActivity, loadingInitial} = activityStore;
+    const {createActivity, updateActivity,
+           loading, loadActivity, loadingInitial} = activityStore;
 
     // const navigate = useNavigate();
 
     const {id} = useParams();
+
+    const navigate = useNavigate();
 
     const [activity, setActivity] = useState<Activity>({
         id: '',
@@ -45,14 +49,14 @@ export default observer(function ActivityForm() {
         if (id) loadActivity(id).then(activity => setActivity(activity!));
     },[id, loadActivity]);
 
-    // function handleSubmit() { 
-    //     if (!activity.id) {
-    //         activity.id = uuid();
-    //         createActivity(activity).then(() => navigate(`/activities/${activity.id}`));
-    //     } else {
-    //         updateActivity(activity).then(() => navigate(`/activities/${activity.id}`));
-    //     }
-    // }
+    function handleFormSubmit(activity: Activity) { 
+        if (!activity.id) {
+            activity.id = uuid();
+            createActivity(activity).then(() => navigate(`/activities/${activity.id}`));
+        } else {
+            updateActivity(activity).then(() => navigate(`/activities/${activity.id}`));
+        }
+    }
 
     // function handleChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
     //     const { name, value } = event.target;
@@ -63,12 +67,13 @@ export default observer(function ActivityForm() {
 
     return (
         <Segment clearing>
+            <Header content='Activity Details' sub color='teal' />
             <Formik 
                 validationSchema={validationSchema}
                 enableReinitialize 
                 initialValues={activity} 
-                onSubmit={values => console.log(values)}>
-                {({ handleSubmit}) => (
+                onSubmit={values => handleFormSubmit(values)}>
+                {({handleSubmit, isValid, isSubmitting, dirty}) => (
                 <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
           
                     <MyTextInput name='title' placeholder='Title' />
@@ -80,7 +85,9 @@ export default observer(function ActivityForm() {
                     <MyTextInput name='venue' placeholder='Venue' />
                     <MyTextInput name='city' placeholder='city' />
                     
-                    <Button loading={loading} floated='right' positive type='submit' content='Submit' />
+                    <Button 
+                        disabled={isSubmitting || !dirty || !isValid}
+                        loading={loading} floated='right' positive type='submit' content='Submit' />
                     <Button as={Link} to='/activities' floated='right' type='button' content='Cancel' />
                 </Form>
                 )}
