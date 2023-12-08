93e6805f34f2ba7a45467e4b08e6b7a4df032cca
lufie <lufie@lufiedeMacBook-Air.local>
2023-12-08 21:15:58 +0800

    register

--- client-app/src/app/stores/modalStore.ts
+++ client-app/src/app/stores/modalStore.ts
@@ -16,7 +16,6 @@ export default class ModalStore {
     }
 
     openModal = (content: JSX.Element) => {
-        console.log("!!!");
         this.modal.open = true;
         this.modal.body = content;
     }
