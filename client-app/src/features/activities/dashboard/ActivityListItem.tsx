652e33ef94492cdc4ec0c2597427622965a4b614
lufie <lufie@lufiedeMacBook-Air.local>
2023-12-12 19:53:42 +0800

    cancel activity

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -12,6 +12,9 @@ export default function ActivityListItem({activity}: Props) {
     return (
         <SegmentGroup>
             <Segment>
+                {activity.isCancelled &&
+                    <Label attached='top' color='red' 
+                        content='Cancelled' style={{ textAlign: 'center' }} />}
                 <ItemGroup>
                     <Item>
                         <Item.Image size="tiny" circular src='/assets/user.png' />
